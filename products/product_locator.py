6b60b92a6be912bbebb133787e281926bb5f3ba8
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2020-08-31 23:42:59 +0100

    Add files via upload

---
+++ products/product_locator.py
@@ -0,0 +1,7 @@
+from web_book_scrapper.products import product_elements
+
+
+class ProductParserLocator(object):
+    """The class uses the selector retreive any elements associated with the product class"""
+    BOOKS_LI = product_elements.BOOKS_UNORDER_LIST
+
