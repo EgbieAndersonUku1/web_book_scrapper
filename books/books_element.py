000b86a352a299d30c1f2a99fbe3313c539e5e51
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2020-08-31 23:49:00 +0100

    Add files via upload

---
+++ books/books_element.py
@@ -0,0 +1,26 @@
+
+# the book locator selector for all the book on the first page
+BOOKS = "#default > div > div > div > div > section > div:nth-child(2) > ol > li"
+
+
+# the title locator for a given book
+TITLE = "#content_inner > article > div.row > div.col-sm-6.product_main > h1"
+
+
+# the price locator for a given book
+PRICE = "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color"
+
+# the image locator link for a given book
+IMAGE = "#product_gallery > div > div > div > img"
+
+
+# the description locator for a given book
+DESCRIPTION = "#content_inner > article > p"
+
+# the rating locator for a given book
+RATING = "#content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating"
+
+
+# the tag headings for a given book
+TABLE_ROW_HEADING_TAG = "th"
+TABLE_ROW_DATA_TAG = "td"
