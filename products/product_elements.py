3769d86f2cb0076452734fda940f17c42093efbb
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2020-08-31 23:41:51 +0100

    Add files via upload

---
+++ products/product_elements.py
@@ -0,0 +1,2 @@
+# The selector uses to retreive the elements for all the books on the first page
+BOOKS_UNORDER_LIST = "#default > div > div > div > div > section > div > ol > li > article > div.image_container > a"
