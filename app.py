c2a4c54f1ada714733fb74d604fe9208206a3fe0
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2020-08-31 23:38:59 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+
+from bs4 import BeautifulSoup
+from web_book_scrapper.menu.menu_parser import MenuParser
+from web_book_scrapper.products.product_parser import ProductParser
+
+
+class BooksToScrapePage(object):
+
+    def __init__(self, url):
+        self.url = url
+        self._content = None
+        self._soup_handler = None
+
+    def initalize(self):
+        """Initialise the class. This must be run before any off the properties are called"""
+        page = requests.get(self.url)
+        if page:
+            self._soup_handler = BeautifulSoup(page.content, "html.parser")
+            self._content = page.content
+
+    @property
+    def menu(self):
+        """Returns a list of menu categories found on the page"""
+        return MenuParser(self._soup_handler).menu_categories
+
+    def products(self):
+        """Returns a list book objects for the current books found on the page"""
+        return ProductParser(self._soup_handler, self.url)
+
+
+
+# page = BooksToScrapePage("http://books.toscrape.com")
+# page.initalize()
